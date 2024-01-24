31f6b373c6de8f5992612f7840c958dd492fcf1d
Simon <simonguo.eng@yahoo.com>
2024-01-24 15:02:26 -0500

    add loop that print all cell values in range

--- excelAuto.py
+++ excelAuto.py
@@ -1,5 +1,6 @@
 #import pandas as pd
 import openpyxl
+from openpyxl import load_workbook, worksheet, Workbook
 from openpyxl.styles import Font, Color
 
 wb = openpyxl.load_workbook('ProfileVV.xlsx')
@@ -14,4 +15,8 @@ ws['L4']="Index"
 ws['K4'].font=ft
 ws['L4'].font=ft
 
+
 wb.save('ProfileVV1.xlsx')
+for row in ws.iter_cols(min_col=10, max_col=10, min_row=4,max_row=30):
+    for cell in row:
+        print(cell.value)
