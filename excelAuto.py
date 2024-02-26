5b5b61841b47d4adcce1cdadf665ed53d413ad5e
Simon <simonguo.eng@yahoo.com>
2024-02-26 15:50:07 -0500

    add formula to K and L columns

--- excelAuto.py
+++ excelAuto.py
@@ -2,6 +2,7 @@
 import openpyxl
 from openpyxl import load_workbook, worksheet, Workbook
 from openpyxl.styles import Font, Color
+from openpyxl.worksheet.formula import ArrayFormula
 
 wb = openpyxl.load_workbook('ProfileVV.xlsx')
 target = wb['Variable vs Variable']
@@ -11,12 +12,25 @@ print(wb.sheetnames)
 ft=Font(color="FF0000")
 ws=wb['Variable vs Variable Copy']
 ws['K4'] = "%Pen"
-ws['L4']="Index"
-ws['K4'].font=ft
-ws['L4'].font=ft
+ws['L4'] = "Index"
+col1 = ws.column_dimensions['K']
+col1.font=ft
+col2 = ws.column_dimensions['L']
+col2.font=ft
+# Apply formula
+ws["K3"] = ArrayFormula("K5:K72", "=E5:E72/G5:G72*100")    
+ws["L3"] = ArrayFormula("L5:L72", "=F5:F72/H5:H72*100")
 
 
 wb.save('ProfileVV1.xlsx')
-for row in ws.iter_cols(min_col=10, max_col=10, min_row=4,max_row=30):
-    for cell in row:
-        print(cell.value)
+#for row in ws.iter_cols(min_col=10, max_col=10, min_row=4,max_row=30):
+ #   for cell in row:
+  #      print(cell.value)
+
+
+for x in range(4,30):
+    count = ws.cell(row=x, column=5).value
+    base_count = ws.cell(row=x, column=7).value
+    index = ws.cell(row=x, column=10).value 
+    print(count,base_count,index)
+
