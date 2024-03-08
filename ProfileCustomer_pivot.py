39498b425fdfd02b43f67600b2660b3a642f08a4
Simon <simonguo.eng@yahoo.com>
2024-03-08 13:33:29 -0500

    make pivot table works

--- ProfileCustomer_pivot.py
+++ ProfileCustomer_pivot.py
@@ -3,8 +3,10 @@ import matplotlib.pylab as plt
 
 
 #reference workbook
-fileName = "Sample_Customer_File - 5K_W_All_Additional_Types.csv"
+fileName = "Import_COORD_CAN_-_5K_with_all_CTG_Special_Char.csv"
 #headers = ["UniqueID","Postal Code","City","StoreCode","CustomerWeight","Dollars","Member Type","Some Text","Flag"]
 df = pd.read_csv(fileName)
 print(df.head())
-#table = pd.pivot_table(df,)
+table = pd.pivot_table(df[df.__PRIZM!='Not Coded'],values='Count',index='__PRIZM',aggfunc="sum")
+print(table)
+table.to_csv("pivot_table.csv")
